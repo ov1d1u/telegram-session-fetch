bca67a40419b571bd79a900d03d7996429b0ec37
Ovidiu Nitan <nitanovidiu@gmail.com>
2023-11-08 23:28:57 +0200

    Update session name

--- main.py
+++ main.py
@@ -4,5 +4,5 @@ SESSION_FILE = 'eq_alert.session'
 TELEGRAM_API_ID = 123456
 TELEGRAM_API_HASH = "1234567890abcdef1234567890abcdef"
 
-client = TelegramClient('session_name', TELEGRAM_API_ID, TELEGRAM_API_HASH)
+client = TelegramClient('eq_alert', TELEGRAM_API_ID, TELEGRAM_API_HASH)
 client.start()
